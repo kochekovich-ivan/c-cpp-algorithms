0efe376e413135c1bdda689464748363772e3a14
ivan kochekovych <ethicalhackingivan@gmail.com>
2025-10-04 08:57:01 +0200

    Update ArithmeticFunctionTau.cpp

--- exercises/ArithmeticFunctionTau.cpp
+++ exercises/ArithmeticFunctionTau.cpp
@@ -2,15 +2,16 @@
   Time limit: 0.2 seconds
   Memory limit: 64 megabytes
 
-  The arithmetic function τ : N → N is defined as the number of positive integer divisors of a positive integer n:
-    τ(n) = Σ_{d|n} 1
-  The function τ is multiplicative, meaning that:
-    gcd(m, n) = 1  ⇒  τ(m * n) = τ(m) * τ(n)
-  If the number n can be factorized as:
-     n = p1^s1 * p2^s2 * ... * pr^sr
-  Then, by the multiplicativity property, τ(n) can be calculated as:
-      τ(n) = τ(p1^s1) * τ(p2^s2) * ... * τ(pr^sr)
-           = (s1 + 1) * (s2 + 1) * ... * (sr + 1)
+  Exercise:
+    The arithmetic function τ : N → N is defined as the number of positive integer divisors of a positive integer n:
+      τ(n) = Σ_{d|n} 1
+    The function τ is multiplicative, meaning that:
+      gcd(m, n) = 1  ⇒  τ(m * n) = τ(m) * τ(n)
+    If the number n can be factorized as:
+       n = p1^s1 * p2^s2 * ... * pr^sr
+    Then, by the multiplicativity property, τ(n) can be calculated as:
+        τ(n) = τ(p1^s1) * τ(p2^s2) * ... * τ(pr^sr)
+             = (s1 + 1) * (s2 + 1) * ... * (sr + 1)
 
   Input Format:
     The input data contains a single integer n.
